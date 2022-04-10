67b8c14c0b3bced4c31b6660f8ca9dc3a166a91d
amitkumar <amitkumar.chottu58@gmail.com>
2022-04-10 23:17:00 +0530

    Initial commit

---
+++ scientific_calculator_mini_project/src/main/java/com/iiitb/scientific_calculator_mini_project/ScientificCalculatorMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientific_calculator_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCalculatorMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificCalculatorMiniProjectApplication.class, args);
+    }
+
+}
---
+++ scientific_calculator_mini_project/src/test/java/com/iiitb/scientific_calculator_mini_project/ScientificCalculatorMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientific_calculator_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificCalculatorMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
